15896755d022ee4aa3afb826c11d238608dd4291
zorasantos <zoranildo22@gmail.com>
2023-04-07 10:38:09 -0300

    config: tailwindcss

--- src/components/HelloWorld.test.ts
+++ src/components/HelloWorld.test.ts
@@ -11,16 +11,4 @@ describe("HelloWorld", () => {
     });
     expect(wrapper.text()).toContain(msg);
   });
-
-  test("increments count when button is clicked", async () => {
-    const msg = "Hello world";
-    const wrapper = mount(HelloWorld, {
-      props: {
-        msg,
-      },
-    });
-    const button = wrapper.find("button");
-    await button.trigger("click");
-    expect(wrapper.text()).toContain("count is 1");
-  });
 });
